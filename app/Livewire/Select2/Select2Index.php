a74538c8e716597ab411eba33bfea4ae7fa054a1
dressrahmat <dedysrahmat97@gmail.com>
2024-03-28 13:40:20 +0700

    select2 (remote data)

--- app/Livewire/Select2/Select2Index.php
+++ app/Livewire/Select2/Select2Index.php
@@ -9,6 +9,24 @@ class Select2Index extends Component
 {
     public $city = 'CF';
     public $customer = 15;
+
+    public function fetchData($val)
+    {
+        $data_customer = [];
+
+        $query = Customer::where('name', 'like', '%' . $val . '%')->get();
+
+        if (isset($query)) {
+            foreach ($query as $key => $value) {
+                $data_customer[$key] = [
+                    'id' => $value->id,
+                    'name' => $value->name
+                ];
+            }
+        }
+
+        $this->dispatch('set-data-customer', data_customer: $data_customer, val: $val);
+    }
     public function render()
     {
         $customers = Customer::get();
